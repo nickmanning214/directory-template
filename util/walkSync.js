7acbac5f78077e45d77727d5e244e21e787ffd43
Nick Manning <nickmanning214@gmail.com>
2020-04-05 14:04:23 -0400

    make initial compiled prototype folder

---
+++ util/walkSync.js
@@ -0,0 +1,18 @@
+module.exports = function walkSync(dir, filelist) {
+
+    if( dir[dir.length-1] != '/') dir=dir.concat('/')
+
+  
+    var fs = fs || require('fs'),
+    files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+      if (fs.statSync(dir + file).isDirectory()) {
+        filelist = walkSync(dir + file + '/', filelist);
+      }
+      else {
+        filelist.push(dir+file);
+      }
+    });
+    return filelist;
+  };
